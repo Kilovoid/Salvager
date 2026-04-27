081965cccd0481abf54312964e37fda77ce8e348
Kilovoid <soma.savin@gmail.com>
2026-04-27 23:44:05 +0300

    single note functionality

---
+++ src/com/kilovoid/salvager/Note.java
@@ -0,0 +1,43 @@
+package com.kilovoid.salvager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Note {
+    private String title;
+    private String content;
+
+    //Constructor
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+
+    public void saveToFile(String dirPath) throws IOException {
+        String fileName = title.replaceAll("\\s+", "_") + ".txt";
+        Path path = Path.of(dirPath, fileName);
+        String fileContent = title + "\n\n" + content;
+        Files.writeString(path, fileContent);
+    }
+
+    public static Note loadFromFile(String filePath) throws IOException {
+        Path path = Path.of(filePath);
+        String content = Files.readString(path);
+        String[] lines = content.split("\n",3);
+        if (lines.length < 2) {
+            throw new IOException("Invalid file format");
+        }
+        String title = lines[0].trim();
+        String body = lines.length > 2 ? lines[2] : "";
+
+        return new Note(title, body);
+    }
+}
