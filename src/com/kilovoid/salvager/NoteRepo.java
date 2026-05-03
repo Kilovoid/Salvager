9fa7c556a3f20151e831efeb82ed6311187bc1a3
Kilovoid <soma.savin@gmail.com>
2026-05-03 17:14:11 +0300

    Added NoteRepo class

---
+++ src/com/kilovoid/salvager/NoteRepo.java
@@ -0,0 +1,28 @@
+/// Этот класс отвечает за то, чтобы какая-то константа, указанная в процессе выбора папки сохранения всех записей
+/// была обработана как директория. То есть здесь должно быть поле пути, потом конструктор, потому что
+/// зачастую у нас будет новая папка, типовая, например "D:\MyFiles" - в результате обработки такого пути там будет
+/// создана папка "SalvageNotes", например (D:\MyFiles\SalvageNotes)
+
+package com.kilovoid.salvager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class NoteRepo {
+    private final Path repoPath;
+
+    public NoteRepo(String selectedPath) {
+        Path basePath = Path.of(selectedPath); /// Вот про это я писал - то есть это выбранная папка - условная MyFiles
+        this.repoPath = basePath.resolve("SalvageNotes"); /// .resolve() здесь будет присоединять к basePath дефолтный путь SalvageNotes
+        try {
+            Files.createDirectories(repoPath);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't create a new notes repo!", e);
+        }
+    }
+
+    public Path getRepoPath() {
+        return repoPath;
+    }
+}
