21d60fa3887e93be2888a44af50de54edf4d8bd7
Kilovoid <soma.savin@gmail.com>
2026-08-16 17:19:48 +0300

    Left Alt now doesn't trigger different UI elements selection

--- Salvager/Views/MainWindow.axaml.cs
+++ Salvager/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Salvager.ViewModels;
 
 namespace Salvager.Views;
@@ -8,5 +10,18 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        this.AddHandler(InputElement.KeyDownEvent, OnPreviewKeyDown, RoutingStrategies.Tunnel);
+    }
+
+    private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        var focused = FocusManager.GetFocusedElement();
+        if (focused is TextBox)
+        {
+            if(e.Key == Key.LeftAlt)
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
