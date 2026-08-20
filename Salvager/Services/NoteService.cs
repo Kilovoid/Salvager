492ad64263437a2a04318cfa5b87b16458bfeedf
Kilovoid <soma.savin@gmail.com>
2026-08-20 15:07:05 +0300

    Deleted dead code

--- Salvager/Services/NoteService.cs
+++ Salvager/Services/NoteService.cs
@@ -165,21 +165,8 @@ namespace Salvager.Services
                 }
             }
             throw new FileNotFoundException($"Note with ID {noteId} does not exist");
-            //string fileName = noteId + ".md";
-            //string[] matchingFiles = Directory.GetFiles(_notesDirectory, fileName);
-            //if (matchingFiles.Length == 0)
-            //{
-            //    throw new FileNotFoundException($"Note with id {noteId} is not found in {_notesDirectory}");
-            //}
-            //string filePath = matchingFiles[0];
-            //using var reader = new StreamReader(filePath);
-            //string title = reader.ReadLine() ?? "Untitled";
-            //string content = reader.ReadToEnd();
-            //Note loadedNote = new Note(noteId, title, content,
-            //    File.GetCreationTime(filePath), File.GetLastWriteTime(filePath));
-            //return loadedNote;
         }
-        public void SaveNote(Note currentNote) // Надо переписывать заголовок обязательно
+        public void SaveNote(Note currentNote)
         {
             if (currentNote == null)
             {
