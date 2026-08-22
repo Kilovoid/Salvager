9ab0478bf19e679bb7b8d049593a7ac3b88dfff7
Kilovoid <soma.savin@gmail.com>
2026-08-22 12:32:50 +0300

    Added IFileSystem and RealFileSystem for later development, mocking and testing

---
+++ Salvager/Models/IFileSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salvager.Models
+{
+    internal interface IFileSystem
+    {
+        bool DirectoryExists(string path);
+        bool FileExists(string path);
+        string[] GetFiles(string path, string pattern);
+        void WriteAllText(string path, string contents, Encoding encoding);
+        string ReadAllText(string path);
+        void CreateDirectory(string path);
+        void DeleteFile(string path);
+    }
+}
---
+++ Salvager/Models/RealFileSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Salvager.Models
+{
+    internal class RealFileSystem : IFileSystem
+    {
+        public void CreateDirectory(string path) => Directory.CreateDirectory(path);
+
+        public void DeleteFile(string path) => File.Delete(path);
+
+        public bool DirectoryExists(string path) => Directory.Exists(path);
+        public bool FileExists(string path) => File.Exists(path);
+
+        public string[] GetFiles(string path, string pattern) => Directory.GetFiles(path, pattern);
+        public string ReadAllText(string path) => File.ReadAllText(path);
+
+        public void WriteAllText(string path, string contents, Encoding encoding) =>
+            File.WriteAllText(path, contents, encoding);
+    }
+}
