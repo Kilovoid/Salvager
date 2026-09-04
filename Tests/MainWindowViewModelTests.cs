32acd25f2ee2fa634981d0e5ab1db5c2fe225f75
Kilovoid <soma.savin@gmail.com>
2026-09-04 20:33:47 +0300

    test(ViewModel): create MainWindowViewModel test class

---
+++ Tests/MainWindowViewModelTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Salvager.Models;
+using Salvager.Services;
+using Salvager.ViewModels;
+
+namespace Tests
+{
+    public class MainWindowViewModelTests
+    {
+        
+    }
+}
