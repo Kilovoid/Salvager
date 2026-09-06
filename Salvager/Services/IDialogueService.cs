1ee317ce0a2edf936b5925fd1600bb77f08ffa78
Kilovoid <soma.savin@gmail.com>
2026-09-06 12:05:46 +0300

    service: add an interface and a service for dialogue

---
+++ Salvager/Services/IDialogueService.cs
@@ -0,0 +1,14 @@
+﻿using MsBox.Avalonia.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salvager.Services
+{
+    public interface IDialogueService
+    {
+        Task<ButtonResult> ShowWarningAsync(string title, string message, ButtonEnum buttons = ButtonEnum.Ok);
+        Task ShowErrorAsync(string title, string message);
+    }
+}
---
+++ Salvager/Services/RealDialogueService.cs
@@ -0,0 +1,24 @@
+﻿using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salvager.Services
+{
+    internal class RealDialogueService : IDialogueService
+    {
+        public async Task ShowErrorAsync(string title, string message)
+        {
+            var box = MessageBoxManager.GetMessageBoxStandard(title, message, ButtonEnum.Ok);
+            await box.ShowAsync();
+        }
+
+        public async Task<ButtonResult> ShowWarningAsync(string title, string message, ButtonEnum buttons = ButtonEnum.Ok)
+        {
+            var box = MessageBoxManager.GetMessageBoxStandard(title, message, buttons);
+            return await box.ShowAsync();
+        }
+    }
+}
